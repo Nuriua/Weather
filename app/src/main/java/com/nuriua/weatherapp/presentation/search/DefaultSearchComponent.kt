bbe89585d8b6819116fc8c877d90f2b2850a8721
Nuriya Akhmedshina <nyriya36@gmail.com>
2025-08-22 14:28:51 +0500

    components realisation

--- app/src/main/java/com/nuriua/weatherapp/presentation/search/DefaultSearchComponent.kt
+++ app/src/main/java/com/nuriua/weatherapp/presentation/search/DefaultSearchComponent.kt
@@ -1,7 +1,63 @@
 package com.nuriua.weatherapp.presentation.search
 
 import com.arkivanov.decompose.ComponentContext
+import com.arkivanov.mvikotlin.core.instancekeeper.getStore
+import com.nuriua.weatherapp.domain.entity.City
+import com.nuriua.weatherapp.presentation.extensions.componentScope
+import com.arkivanov.mvikotlin.extensions.coroutines.labels
+import com.arkivanov.mvikotlin.extensions.coroutines.stateFlow
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class DefaultSearchComponent(
+class DefaultSearchComponent @Inject constructor(
+    private val openReason: OpenReason,
+    private val storeFactory: SearchStoreFactory,
+    private val onBackClicked: () -> Unit,
+    private val onCitySavedToFavorite: () -> Unit,
+    private val onForecastForCitySavedRequested: (City) -> Unit,
     componentContext: ComponentContext
-) : SearchComponent, ComponentContext by componentContext
+) : SearchComponent, ComponentContext by componentContext {
+
+    private val store = instanceKeeper.getStore { storeFactory.create(openReason) }
+    private val scope = componentScope()
+
+    init {
+        scope.launch {
+            store.labels.collect {
+                when(it) {
+                    SearchStore.Label.ClickBack -> {
+                        onBackClicked
+                    }
+                    is SearchStore.Label.OpenForecast -> {
+                        onForecastForCitySavedRequested(it.city)
+                    }
+                    SearchStore.Label.SavedToFavourite -> {
+                        onCitySavedToFavorite
+                    }
+                }
+            }
+        }
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    override val model: StateFlow<SearchStore.State> = store.stateFlow
+
+    override fun changeSearchQuery(query: String) {
+        store.accept(SearchStore.Intent.ChangeSearchQuery(query))
+    }
+
+    override fun onClickBack() {
+        store.accept(SearchStore.Intent.ClickBack)
+    }
+
+    override fun onClickSearch() {
+        store.accept(SearchStore.Intent.ClickSearch)
+    }
+
+    override fun onClickCity(city: City) {
+        store.accept(SearchStore.Intent.ClickCity(city))
+    }
+}
--- app/src/main/java/com/nuriua/weatherapp/presentation/search/SearchComponent.kt
+++ app/src/main/java/com/nuriua/weatherapp/presentation/search/SearchComponent.kt
@@ -1,4 +1,17 @@
 package com.nuriua.weatherapp.presentation.search
 
+import com.nuriua.weatherapp.domain.entity.City
+import kotlinx.coroutines.flow.StateFlow
+
 interface SearchComponent {
+
+    val model: StateFlow<SearchStore.State>
+
+    fun changeSearchQuery(query: String)
+
+    fun onClickBack()
+
+    fun onClickSearch()
+
+    fun onClickCity(city: City)
 }
